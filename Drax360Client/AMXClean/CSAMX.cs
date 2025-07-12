9195e43e00951c7363095a6810384c1415f1ed70
Richard Jones <richard@binaryrefinery.com>
2025-07-12 11:11:10 +0100

    Changes to stuff as referecenced in progress 5

--- Drax360Client/AMXClean/CSAMX.cs
+++ Drax360Client/AMXClean/CSAMX.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+
 
 namespace Drax360Client.AMXClean
 {
@@ -10,10 +7,13 @@ namespace Drax360Client.AMXClean
     {
         #region constants
         private const int kstartingfilenumber = 9000;
+        private const string kgenextension = ".GEN";
+        private const int kmaxfilenumber = 1000000;
         #endregion
 
         #region private variables
-        private int filenum = kstartingfilenumber;
+        private int filenumber = kstartingfilenumber;
+
         private string workingfolder = "";
         private List<NVM> nvms = new List<NVM>();
         #endregion
@@ -22,11 +22,36 @@ namespace Drax360Client.AMXClean
         #region constructor
         public CSAMX()
         {
-            workingfolder = Path.GetDirectoryName(Application.ExecutablePath) + "//Temp//";
+            workingfolder = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath),"Temp");
         }
         #endregion
 
         #region public methods
+
+        public int IncrementInputNumber(int inputNumber)
+        {
+            return (int) (inputNumber + 0x80000000);
+        }
+        public int MakeInputNumber(int node, int zone, int inputn, int inputtype)
+        { 
+            
+            return inputn + (zone * 0x100) + (node * 0x10000) + (inputtype * 0x8000000);
+        }
+
+        public void WriteNWMData(int eventtype, int eventnumber,
+             string textparameter, string textparameter2, string textparameter3, bool on)
+        {
+            NVM ournvm = new NVM();
+            ournvm.OurType = eventtype;
+            ournvm.OurEvent = eventnumber;
+            ournvm.OnOff = on ? -1 : 0;
+
+            ournvm.Text = textparameter;
+            ournvm.Text2 = textparameter2;
+            ournvm.Text3 = textparameter3;
+
+            nvms.Add(ournvm);
+        }
         public void LogMessage(int eventtype, int eventnumber, string text, int ophandle)
         {
             NVM ournvm = new NVM();
@@ -37,22 +62,39 @@ namespace Drax360Client.AMXClean
             nvms.Add(ournvm);
         }
 
+
         public void FlushMessages()
         {
             if (nvms.Count == 0) return;
 
 
-            filenum++;
             string contents = "";
             foreach (NVM ournvm in nvms)
             {
                 contents += ournvm.Render();
             }
-            string filename = filenum.ToString() + ".GEN";
-            string fullfilename = workingfolder + filename;
 
-            File.WriteAllText(fullfilename, contents);
-            if (filenum > 1000000) filenum = kstartingfilenumber;
+            // safety check
+            if (String.IsNullOrEmpty(contents))
+            {
+                nvms.Clear();
+                return;
+            }
+
+            filenumber++;
+            string filename = filenumber.ToString() + kgenextension;
+            string fullfilename = Path.Combine(workingfolder, filename);
+
+            // Open the file in append mode
+            using (FileStream fileStream = new FileStream(fullfilename, FileMode.Append, FileAccess.Write))
+            {
+                using (StreamWriter writer = new StreamWriter(fileStream))
+                {
+                    writer.Write(contents);
+                }
+            }
+
+            if (filenumber > kmaxfilenumber) filenumber = kstartingfilenumber;
             nvms.Clear();
         }
         #endregion
---
+++ Read AMX File/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "1.GEN";
+
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine("File not found.");
+            return;
+        }
+
+        byte[] bytes = File.ReadAllBytes(filePath);
+
+        if (bytes.Length < 48)
+        {
+            Console.WriteLine("File too short to contain timestamp at offset 44.");
+            return;
+        }
+
+        // Read timestamp from offset 44
+        int timestamp = BitConverter.ToInt32(bytes, 44); // assumes little-endian
+        DateTimeOffset utc = DateTimeOffset.FromUnixTimeSeconds(timestamp);
+        DateTimeOffset local = utc.ToLocalTime();
+
+        Console.WriteLine($"Timestamp (raw): {timestamp}");
+        Console.WriteLine($"UTC:   {utc}");
+        Console.WriteLine($"Local: {local}");
+    }
+}
